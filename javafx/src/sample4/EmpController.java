42a7c84738a8785fda0cacc38d220d81d13effb4
ChoiSJ <lycoricera@gmail.com>
2016-12-28 21:38:36 +0900

    study

--- javafx/src/sample4/EmpController.java
+++ javafx/src/sample4/EmpController.java
@@ -21,7 +21,7 @@ public class EmpController {
 	@FXML
 	private TextField jobIdField;
 	@FXML
-	private TextField hireDateField;
+	private TextField hireDateField;	
 	@FXML
 	private TextField salaryField;
 	@FXML
@@ -29,24 +29,53 @@ public class EmpController {
 	
 	@FXML
 	private void search() throws SQLException {
-		int empId = Integer.parseInt(searchField.getText());
-		
-		Alert alert = new Alert(AlertType.INFORMATION);
-		alert.setTitle("검색 완료");
-		alert.setContentText("검색이 완료되었습니다.");
-		alert.showAndWait();
-		
-		EmpVO emp = ed.employee(empId);
-		
-		empIdField.setText(String.valueOf(emp.getEmpId()));
-		empNameField.setText(emp.getEmpName());
-		jobIdField.setText(emp.getJobId());
-		hireDateField.setText(String.valueOf(emp.getHireDate()));
-		
-		DecimalFormat df = new DecimalFormat("#,###");
-		salaryField.setText(df.format(emp.getSalary()));
-		depIdField.setText(String.valueOf(emp.getDepId()));
-		
+		try {
+			int empId = Integer.parseInt(searchField.getText());
+			EmpVO emp = ed.employee(empId);
+			
+			if (emp == null) {
+				Alert alert1 = new Alert(AlertType.INFORMATION);
+				alert1.setTitle("검색 완료");
+				alert1.setHeaderText(null);
+				alert1.setContentText("검색 결과가 없습니다.");
+				alert1.showAndWait();
+				clear();
+				return;
+			}
+			
+			Alert alert2 = new Alert(AlertType.INFORMATION);
+			alert2.setTitle("검색 완료");
+			alert2.setHeaderText(null);
+			alert2.setContentText("검색이 완료되었습니다.");
+			alert2.showAndWait();
+			
+			empIdField.setText(String.valueOf(emp.getEmpId()));
+			empNameField.setText(emp.getEmpName());
+			jobIdField.setText(emp.getJobId());
+			hireDateField.setText(String.valueOf(emp.getHireDate()));
+			
+			DecimalFormat df = new DecimalFormat("#,###");
+			salaryField.setText(df.format(emp.getSalary()));
+			depIdField.setText(String.valueOf(emp.getDepId()));
+			
+			searchField.setText("");
+		} catch (NumberFormatException e) {
+			Alert alert3 = new Alert(AlertType.ERROR);
+			alert3.setTitle("검색 완료");
+			alert3.setHeaderText(null);
+			alert3.setContentText("잘못된 입력입니다.");
+			alert3.showAndWait();
+			clear();
+		}
+	}
+	
+	private void clear() {
 		searchField.setText("");
+		empIdField.setText("");
+		empNameField.setText("");
+		jobIdField.setText("");
+		hireDateField.setText("");
+		salaryField.setText("");
+		depIdField.setText("");
 	}
 }
