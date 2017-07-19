1d5c7898ac737626c260b3f106ad14053696da02
ChoiSJ <lycoricera@gmail.com>
2017-07-19 13:35:37 +0900

    study

--- lyco_study/src/hangman/HangmanController.java
+++ lyco_study/src/hangman/HangmanController.java
@@ -26,7 +26,6 @@ public class HangmanController {
 	private String visibleVoca;
 	private int index;
 	
-	
 	@FXML
 	public void searchHangman(ActionEvent event) {
 		Button btn = (Button) event.getSource();
